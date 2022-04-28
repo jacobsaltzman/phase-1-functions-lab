b1653026bbbe406eb580ece4bbfde15ae5093c84
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-04-27 19:46:20 -0700

    commit tree dbb4178dca0aafdc82be8e414178ee63d0df1d62

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    if (location>=42){
+        return location - 42;
+    }
+    else{
+        return 42 - location;
+    }
+}
+
+function distanceFromHqInFeet(location){
+    const feet = distanceFromHqInBlocks(location) * 264;
+    return feet;
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (start >= destination){
+        return (start - destination) * 264;
+    }
+    else{
+        return (destination - start) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    const travelledFeet = distanceTravelledInFeet(start, destination);
+        if (travelledFeet > 2500){
+            return 'cannot travel that far';
+        }
+        if (travelledFeet < 400){
+            return 0;
+        }
+        else if (travelledFeet < 2000){
+            return (travelledFeet - 400) * .02;
+        }
+        else if (travelledFeet < 2,500){
+            return 25;
+        }
+        
+    }
